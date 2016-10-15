4c33a9f329710be593430621075717f211dbb483
CristianoNunes <cristianonunes9@hotmail.com>
2016-10-14 22:33:20 -0300

    Criado arquivo index e configurado url

--- mysite/PersonalGym/urls.py
+++ mysite/PersonalGym/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from .views import aluno
+from .views import index
+
 
 urlpatterns = [
+    url(r'^$', index, name='index'),
     url(r'^aluno/adicionar$', aluno.adicionar, name='aluno_adicionar'),
     url(r'^aluno/editar/(?P<pk>\d+)$', aluno.editar, name='aluno_editar'),
     url(r'^aluno/apagar/(?P<pk>\d+)$', aluno.apagar, name='aluno_apagar'),
